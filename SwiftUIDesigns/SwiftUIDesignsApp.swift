d3a9bddcb2f9ee4d0d0a20e729b088dad2e88ea5
Edgar <edguitian@imagine800.es>
2023-12-13 12:19:39 +0100

    Initial Commit

---
+++ SwiftUIDesigns/SwiftUIDesignsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDesignsApp.swift
+//  SwiftUIDesigns
+//
+//  Created by Edgar Guitian Rey on 13/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDesignsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
