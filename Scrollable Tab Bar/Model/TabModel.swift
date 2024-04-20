4c6e0567f07faffa6b72d019b6bd98b0cdfb1e18
Édgar Guitián Rey <edguitian@gmail.com>
2024-04-20 14:03:22 +0200

    scrollable tab bar

---
+++ Scrollable Tab Bar/Model/TabModel.swift
@@ -0,0 +1,22 @@
+//
+//  TabModel.swift
+//  SwiftUIDesigns
+//
+//  Created by Edgar Guitian Rey on 20/4/24.
+//
+
+import SwiftUI
+
+struct TabModel: Identifiable {
+    private(set) var id: Tab
+    var size: CGSize = .zero
+    var minX: CGFloat = .zero
+    
+    enum Tab: String, CaseIterable {
+        case research = "Research"
+        case deployment = "Development"
+        case analytics = "Analytics"
+        case audience = "Audience"
+        case privacy = "Privacy"
+    }
+}
