96a47fd48d51ce4c7cf12035e56b549b43ae25ba
Edgar <edguitian@imagine800.es>
2024-07-12 12:55:28 +0200

    [Clean] Modified InspectorView to add multiple options

--- SwiftUIDesigns/Inspector/Inspector View.swift
+++ SwiftUIDesigns/Inspector/Inspector View.swift
@@ -8,19 +8,72 @@
 import SwiftUI
 
 struct Inspector_View: View {
+    let friends = ["Antoine", "Bas", "Curt", "Dave", "Erica"]
+
+    @State private var showInspector = false
     @State private var isShowingInspector = false
 
-        var body: some View {
-            Button("Show Inspector") {
-                isShowingInspector.toggle()
-            }
-            .buttonStyle(.bordered)
-            .font(.largeTitle)
-            .inspector(isPresented: $isShowingInspector) {
-                Text("Inspector View")
-                    .inspectorColumnWidth(min: 50, ideal: 150, max: 200)
-            }
+    var body: some View {
+      VStack {
+          List {
+              ForEach(friends, id: \.self) { friend in
+                  makeCustomerItem(friend)
+                      .onTapGesture {
+                          showInspector = true
+                      }
+                        .inspector(isPresented: $showInspector) {
+                          CustomerDetailView(friend)
+                            .presentationDetents([.height(650), .medium, .large])
+                            .presentationBackgroundInteraction(.disabled)
+                            .inspectorColumnWidth(min: 300, ideal: 300, max: 400)
+                            .toolbar {
+                              Spacer()
+                              Button {
+                                showInspector.toggle()
+                              } label: {
+                                Label("Toggle Inspector", systemImage: "xmark.circle")
+                              }
+                            }
+                        }
+                  
+              }
+
+          }
+          
+          Button("Show Inspector") {
+              isShowingInspector.toggle()
+          }
+          .buttonStyle(.bordered)
+          .font(.largeTitle)
+          .inspector(isPresented: $isShowingInspector) {
+              Text("Inspector View")
+                  .inspectorColumnWidth(min: 50, ideal: 150, max: 200)
+          }
+      }
+    }
+    
+    @ViewBuilder
+    func makeCustomerItem(_ friend: String) -> some View {
+        VStack {
+            Text(friend)
+                .font(.callout.bold())
+            
+            Text("Data")
+            Spacer()
         }
+    }
+    
+    @ViewBuilder
+    func CustomerDetailView(_ friend: String) -> some View {
+        VStack {
+            Text(friend)
+                .font(.callout.bold())
+            
+            Text("Data 1")
+            
+            Text("Data 2")
+        }
+    }
 }
 
 #Preview {
